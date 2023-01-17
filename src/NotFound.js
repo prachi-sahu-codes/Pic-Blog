a2581e4edae9360146a34345a09a9bc032fc8d4d
SahuPrachi <prachisahu998@gmail.com>
2023-01-17 20:30:15 +0530

    final commit

--- src/NotFound.js
+++ src/NotFound.js
@@ -4,10 +4,12 @@ const NotFound = () => {
   return (
     <div className="not-found">
       <h1>404</h1>
-      <h2>Sorry</h2>
+      <h2>Oops!!</h2>
+      <p>The page you are looking for doesn't exist.</p>
 
-      <p>That page cannot be found</p>
-      <Link to="/">Back to the homepage...</Link>
+      <Link to="/" className="backHome">
+        Back to Home...
+      </Link>
     </div>
   );
 };
